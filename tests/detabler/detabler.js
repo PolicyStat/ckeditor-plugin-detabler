c338bb14eeb4fef5e89216aa4e5519d050d0f71e
Alex Tsai <caffodian@gmail.com>
2016-08-15 14:26:52 -0700

    fix up first test

--- tests/detabler/detabler.js
+++ tests/detabler/detabler.js
@@ -1,45 +1,52 @@
 /* bender-tags: editor,unit */
 /* bender-ckeditor-plugins: detabler */
 
-(function() {
+(function () {
     bender.editor = {
         config: {
             enterMode: CKEDITOR.ENTER_P
         }
     };
     bender.test({
-        assertHtml: function( expected, actual, msg ) {
-		    assert.areEqual( bender.tools.fixHtml( expected ), bender.tools.fixHtml( actual ), msg );
-	    },
-        'it does not do anything outside of tables': function() {
-            var editor = this.editorBot.editor,
-                input = '<p>foobar^</p>' +
-                    '<p>baz</p>' +
-                    '<table><tbody><td>leave me alone</td></tbody></table>';
-
-            this.editorBot.setHtmlWithSelection(
-                input
-            );
-
-            editor.execCommand( 'detable' );
-
-            this.assertHtml( input, editor.getData(), 'Editor data does not match.' );
+        assertHtml: function (expected, actual, msg) {
+            assert.areEqual(bender.tools.fixHtml(expected), bender.tools.fixHtml(actual), msg);
         },
-        'it converts table cells to p tags': function() {
+        'it does not do anything outside of tables': function () {
             var editor = this.editorBot.editor,
                 startHtml,
                 endHtml;
 
-            startHtml = '<p>foo></p><table><tbody><td>^foo</td><td>bar</td></tbody></table>';
+            startHtml = '<p>foobar^</p>' +
+                '<p>baz</p>' +
+                '<table><tbody><tr><td>leave me alone</td></tr></tbody></table>';
+
+            endHtml = '<p>foobar</p>' +
+                '<p>baz</p>' +
+                '<table><tbody><tr><td>leave me alone</td></tr></tbody></table>';
+
+            this.editorBot.setHtmlWithSelection(
+                startHtml
+            );
+
+            editor.execCommand('detable');
+
+            this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
+        },
+        'it converts table cells to p tags': function () {
+            var editor = this.editorBot.editor,
+                startHtml,
+                endHtml;
+
+            startHtml = '<p>foo</p><table><tbody><tr><td>^foo</td><td>bar</td></tr></tbody></table>';
             endHtml = '<p>foo</p><p>foo</p><p>bar</p>';
 
             this.editorBot.setHtmlWithSelection(
                 startHtml
             );
 
-            editor.execCommand( 'detable' );
+            editor.execCommand('detable');
 
-            this.assertHtml( endHtml, editor.getData(), 'Editor data does not match.' );
+            this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
         }
     });
 })();
