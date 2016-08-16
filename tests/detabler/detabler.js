8d799050233401b87396631160493d03b9a061f6
Alex Tsai <caffodian@gmail.com>
2016-08-16 14:03:28 -0700

    wow crazy space failure

--- tests/detabler/detabler.js
+++ tests/detabler/detabler.js
@@ -3,135 +3,135 @@
 
 (function () {
 	'use strict';
-    bender.editor = {
-        config: {
-            enterMode: CKEDITOR.ENTER_P
-        }
-    };
-    bender.test({
-        assertHtml: function (expected, actual, msg) {
-            assert.areEqual(bender.tools.fixHtml(expected), bender.tools.fixHtml(actual), msg);
-        },
+	bender.editor = {
+		config: {
+			enterMode: CKEDITOR.ENTER_P
+		}
+	};
+	bender.test({
+		assertHtml: function (expected, actual, msg) {
+			assert.areEqual(bender.tools.fixHtml(expected), bender.tools.fixHtml(actual), msg);
+		},
 		setUp: function () {
 			this.command = this.editorBot.editor.getCommand('detable');
 		},
-        'it does not do anything outside of tables': function () {
-            var editor = this.editorBot.editor,
-                startHtml,
-                endHtml;
+		'it does not do anything outside of tables': function () {
+			var editor = this.editorBot.editor,
+				startHtml,
+				endHtml;
 
-            startHtml = '<p>foobar^</p>' +
-                '<p>baz</p>' +
-                '<table>' +
+			startHtml = '<p>foobar^</p>' +
+				'<p>baz</p>' +
+				'<table>' +
 					'<tbody>' +
 						'<tr><td>leave me alone</td></tr>' +
 					'</tbody>' +
 				'</table>';
 
-            endHtml = '<p>foobar</p>' +
-                '<p>baz</p>' +
-                '<table>' +
+			endHtml = '<p>foobar</p>' +
+				'<p>baz</p>' +
+				'<table>' +
 					'<tbody>' +
 						'<tr><td>leave me alone</td></tr>' +
 					'</tbody>' +
 				'</table>';
 
-            this.editorBot.setHtmlWithSelection(
-                startHtml
-            );
+			this.editorBot.setHtmlWithSelection(
+				startHtml
+			);
 			// setHtmlWithSelection doesn't appear to refresh command state, so we must manually do it
 			this.command.refresh(editor, editor.elementPath());
 
 			assert.areEqual(CKEDITOR.TRISTATE_OFF, this.command.state);
 
-            editor.execCommand('detable');
+			editor.execCommand('detable');
 
-            this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
-        },
-        'it converts table cells to p tags': function () {
-            var editor = this.editorBot.editor,
-                startHtml,
+			this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
+		},
+		'it converts table cells to p tags': function () {
+			var editor = this.editorBot.editor,
+				startHtml,
 				startHtmlWithoutSelection,
-                endHtml;
+				endHtml;
 
-            startHtml = '<p>foo</p>' +
+			startHtml = '<p>foo</p>' +
 				'<table>' +
 					'<tbody>' +
 						'<tr><td>^foo</td><td>bar</td></tr>' +
 					'</tbody>' +
 				'</table>';
 			startHtmlWithoutSelection = '<p>foo</p><table><tbody><tr><td>foo</td><td>bar</td></tr></tbody></table>';
-            endHtml = '<p>foo</p><p>foo</p><p>bar</p>';
+			endHtml = '<p>foo</p><p>foo</p><p>bar</p>';
 
-            this.editorBot.setHtmlWithSelection(
-                startHtml
-            );
+			this.editorBot.setHtmlWithSelection(
+				startHtml
+			);
 			// setHtmlWithSelection doesn't appear to refresh command state, so we must manually do it
 			this.command.refresh(editor, editor.elementPath());
 
 			assert.areEqual(CKEDITOR.TRISTATE_ON, this.command.state);
 
-            editor.execCommand('detable');
+			editor.execCommand('detable');
 
-            this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
+			this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
 
 			editor.execCommand('undo');
 
 			this.assertHtml(startHtmlWithoutSelection, editor.getData(), 'Editor data does not match after being undone.');
-        },
+		},
 		'it does not create nested p': function () {
-            var editor = this.editorBot.editor,
-                startHtml,
-                endHtml;
+			var editor = this.editorBot.editor,
+				startHtml,
+				endHtml;
 
-            startHtml = '<table>' +
+			startHtml = '<table>' +
 					'<tbody>' +
 						'<tr><td><p>^foo</p></td><td>bar</td></tr>' +
 					'</tbody>' +
 				'</table>';
-            endHtml = '<p>foo</p><p>bar</p>'; // the p tag for bar gets created because you can't have a parentless text node
+			endHtml = '<p>foo</p><p>bar</p>'; // the p tag for bar gets created because you can't have a parentless text node
 
-            this.editorBot.setHtmlWithSelection(
-                startHtml
-            );
+			this.editorBot.setHtmlWithSelection(
+				startHtml
+			);
 			// setHtmlWithSelection doesn't appear to refresh command state, so we must manually do it
 			this.command.refresh(editor, editor.elementPath());
 
 			assert.areEqual(CKEDITOR.TRISTATE_ON, this.command.state);
 
-            editor.execCommand('detable');
+			editor.execCommand('detable');
 
-            this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
-        },
+			this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
+		},
 		'it handles multiple non-text children in the same cell': function () {
 			var editor = this.editorBot.editor,
-                startHtml,
-                endHtml;
+				startHtml,
+				endHtml;
 
-            startHtml = '<table>' +
+			startHtml = '<table>' +
 					'<tbody>' +
 						'<tr><td><p>^foo</p><p>bar</p></td></tr>' +
 				'</tbody>' +
 			'</table>';
-            endHtml = '<p>foo</p><p>bar</p>'; // the p tag for bar gets created because you can't have a parentless text node
+			endHtml = '<p>foo</p><p>bar</p>'; // the p tag for bar gets created because you can't have a parentless text node
 
-            this.editorBot.setHtmlWithSelection(
-                startHtml
-            );
+			this.editorBot.setHtmlWithSelection(
+				startHtml
+			);
 			// setHtmlWithSelection doesn't appear to refresh command state, so we must manually do it
 			this.command.refresh(editor, editor.elementPath());
 
 			assert.areEqual(CKEDITOR.TRISTATE_ON, this.command.state);
 
-            editor.execCommand('detable');
+			editor.execCommand('detable');
 
-            this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
+			this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
 		},
 		'it does not break up un-normalized text nodes': function () {
 			// this case is pretty ugly to set up, since you can't express neighboring textnodes as a string.
 			var editor = this.editorBot.editor,
-                startHtml,
-                endHtml,
+				startHtml,
+				endHtml,
 				tds;
 
 			startHtml = '<table>' +
@@ -142,8 +142,8 @@
 			endHtml = '<p>foo</p><p>bar</p>';
 
 			this.editorBot.setHtmlWithSelection(
-                startHtml
-            );
+				startHtml
+			);
 
 			// split foo and bar
 
@@ -155,17 +155,17 @@
 
 			assert.areEqual(CKEDITOR.TRISTATE_ON, this.command.state);
 
-            editor.execCommand('detable');
+			editor.execCommand('detable');
 
-            this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
+			this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
 		},
 		'it only removes the table the selection is in': function () {
-            var editor = this.editorBot.editor,
-                startHtml,
+			var editor = this.editorBot.editor,
+				startHtml,
 				startHtmlWithoutSelection,
-                endHtml;
+				endHtml;
 
-            startHtml = '<table>' +
+			startHtml = '<table>' +
 				'<tbody>' +
 					'<tr><td>' +
 						'<table>' +
@@ -176,23 +176,23 @@
 					'</td></tr>' +
 					'</tbody>' +
 				'</table>';
-            endHtml = '<table>' +
+			endHtml = '<table>' +
 					'<tbody>' +
 						'<tr><td><p>foo</p><p>bar</p></td></tr>' +
 					'</tbody>' +
 				'</table>';
 
-            this.editorBot.setHtmlWithSelection(
-                startHtml
-            );
+			this.editorBot.setHtmlWithSelection(
+				startHtml
+			);
 			// setHtmlWithSelection doesn't appear to refresh command state, so we must manually do it
 			this.command.refresh(editor, editor.elementPath());
 
 			assert.areEqual(CKEDITOR.TRISTATE_ON, this.command.state);
 
-            editor.execCommand('detable');
+			editor.execCommand('detable');
 
-            this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
-        },
-    });
+			this.assertHtml(endHtml, editor.getData(), 'Editor data does not match.');
+		},
+	});
 })();
