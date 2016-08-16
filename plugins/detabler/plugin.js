5b60c35e3f06a13894ab01ad4cee591720743933
Alex Tsai <caffodian@gmail.com>
2016-08-16 14:01:11 -0700

    saving indents like lives

--- plugins/detabler/plugin.js
+++ plugins/detabler/plugin.js
@@ -15,29 +15,31 @@
 					path = editor.elementPath();
 					table = path.contains('table');
 
-					if (table) {
-						table.$.normalize(); // we don't want to deal with un-merged text nodes here.
-						cells = table.find('td, th');
-						// loop through all cell-like items
-						for (var i = 0; i < cells.count(); ++i) {
-							currentCell = cells.getItem(i);
-
-							// we can't use a for loop through `getChildren()`
-							// because we remove children as part of this loop
-							while ((currentItem = currentCell.getChild(0)) !== null) {
-								// we should manually wrap the text nodes in p tags
-								// otherwise we are at the mercy of HTML autofixing
-								// and neighboring cell content may get merged into the same p
-								if (currentItem.type === CKEDITOR.NODE_TEXT) {
-									var wrapper = new CKEDITOR.dom.element('p');
-									currentItem.appendTo(wrapper);
-									currentItem = wrapper;
-								}
-								currentItem.insertBefore(table);
-							}
-						}
-						table.remove();
+					if (!table) {
+						return;
 					}
+					table.$.normalize(); // we don't want to deal with un-merged text nodes here.
+					cells = table.find('td, th');
+					// loop through all cell-like items
+					for (var i = 0; i < cells.count(); ++i) {
+						currentCell = cells.getItem(i);
+
+						// we can't use a for loop through `getChildren()`
+						// because we remove children as part of this loop
+						while ((currentItem = currentCell.getChild(0)) !== null) {
+							// we should manually wrap the text nodes in p tags
+							// otherwise we are at the mercy of HTML autofixing
+							// and neighboring cell content may get merged into the same p
+							if (currentItem.type === CKEDITOR.NODE_TEXT) {
+								var wrapper = new CKEDITOR.dom.element('p');
+								currentItem.appendTo(wrapper);
+								currentItem = wrapper;
+							}
+							currentItem.insertBefore(table);
+						}
+					}
+					table.remove();
+
 				},
 				refresh: function (editor, path) {
 					if (this.state !== CKEDITOR.TRISTATE_DISABLED) {
