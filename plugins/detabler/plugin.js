604f29081752e1b9e3c4cb645dc219e26fd53ccb
Alex Tsai <caffodian@gmail.com>
2016-08-16 07:28:13 -0700

    set command state based on whether we're in a table

--- plugins/detabler/plugin.js
+++ plugins/detabler/plugin.js
@@ -1,9 +1,23 @@
 'use strict';
 
-(function() {
-    CKEDITOR.plugins.add( 'detabler', {    
-    init: function( editor ) {
-        // Plugin logic goes here...
-    }
-});
+(function () {
+    CKEDITOR.plugins.add('detabler', {
+		init: function (editor) {
+			editor.addCommand('detable', {
+				exec: function (editor) {
+					//alert( 'Executing a command for the editor name "' + editor.name + '"!' );
+				},
+				refresh: function (editor, path) {
+					if (this.state !== CKEDITOR.TRISTATE_DISABLED) {
+						if (path.contains('table')) {
+							this.setState(CKEDITOR.TRISTATE_ON);
+						}
+						else {
+							this.setState(CKEDITOR.TRISTATE_OFF);
+						}
+					}
+				}
+			});
+		}
+	});
 })();
